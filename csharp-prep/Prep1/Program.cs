3e6519ec829fef8c3841ea3dabccbb4114659011
Brenner <brenner.mann27@gmail.com>
2024-03-15 14:29:51 -0600

    Finished Prep One as assigned

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("What is your First Name?");
+        string user_first_name=Console.ReadLine();
+        Console.WriteLine("What is Your Last Name?");
+        string user_last_name=Console.ReadLine();
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine($"{user_last_name}, {user_first_name} {user_last_name}");
     }
 }
