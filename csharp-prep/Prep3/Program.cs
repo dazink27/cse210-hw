51a24ba3e8d715e3ff671b6473018c7cbd74b9e2
Brenner <brenner.mann27@gmail.com>
2024-03-15 15:59:59 -0600

    Completed Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.WriteLine("What is your guess?");
+        Random randomGenerator=new Random();
+        int magic_number=randomGenerator.Next(0,10);
+
+
+        string user_Input=Console.ReadLine();
+        int user_Number=int.Parse(user_Input);
+
+        if (user_Number == magic_number)
+        {
+            Console.WriteLine(
+                "That is the magic number!");
+        }
+        else
+        {
+            while (user_Number != magic_number)
+            {
+                if (magic_number > user_Number)
+                {
+                    Console.WriteLine(
+                        "The Magic Number is Higher!");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        "The Magic Number is Lower!");
+                }
+                Console.WriteLine("What is your guess?");
+                user_Input=Console.ReadLine();
+                user_Number=int.Parse(user_Input);
+            }
+        Console.WriteLine(
+            "Congrats! You have the right number!!");
+        }
+        
     }
 }
